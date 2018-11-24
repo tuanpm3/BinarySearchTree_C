e98028c0293bfebf90075af582dd18149e09d036
Pham Minh Tuan <tuanpm@Phams-MacBook-Pro.local>
2018-11-24 15:45:17 +0700

    replace tab = spaces

--- BST.h
+++ BST.h
@@ -1,8 +1,8 @@
 /**
-* @file		BST.h
-* @author	TuanPM3
-* @date		12/Nov/2018
-* @brief	Binary Search Tree template header
+* @file        BST.h
+* @author    TuanPM3
+* @date        12/Nov/2018
+* @brief    Binary Search Tree template header
 */
 
 #ifndef  _BST_H_
@@ -21,8 +21,8 @@ using namespace std;
 template<typename T>
 struct TreeNode
 {
-	T key;
-	struct TreeNode *left, *right;
+    T key;
+    struct TreeNode *left, *right;
 };
 
 /**
@@ -31,254 +31,254 @@ struct TreeNode
 template<typename T>
 class Bst {
 private:
-	TreeNode<T>* mRootNode;
+    TreeNode<T>* mRootNode;
 
-	TreeNode<T>* addInternal(TreeNode<T> *node, T key) {
-		/* If the tree is empty, return a new node */
-		if (node == NULL) return newNode(key);
+    TreeNode<T>* addInternal(TreeNode<T> *node, T key) {
+        /* If the tree is empty, return a new node */
+        if (node == NULL) return newNode(key);
 
-		/* Otherwise, recursive down the tree */
-		if (key < node->key)
-			node->left = addInternal(node->left, key);
-		else if (key > node->key)
-			node->right = addInternal(node->right, key);
+        /* Otherwise, recursive down the tree */
+        if (key < node->key)
+            node->left = addInternal(node->left, key);
+        else if (key > node->key)
+            node->right = addInternal(node->right, key);
 
-		/* return current node pointer */
-		return node;
-	}
+        /* return current node pointer */
+        return node;
+    }
 
-	static TreeNode<T>* newNode(T key) {
-		TreeNode<T> *node = new TreeNode<T>;
-		node->key = key;
-		node->left = NULL;
-		node->right = NULL;
-		return node;
-	}
+    static TreeNode<T>* newNode(T key) {
+        TreeNode<T> *node = new TreeNode<T>;
+        node->key = key;
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+    }
 
 public:
-	class BstIteratorBase : public std::iterator<std::bidirectional_iterator_tag, T> {
-	public:
-		friend class Bst;
-		BstIteratorBase() {};
+    class BstIteratorBase : public std::iterator<std::bidirectional_iterator_tag, T> {
+    public:
+        friend class Bst;
+        BstIteratorBase() {};
 
-		bool operator== (const BstIteratorBase& rhs) const { return mCurrNode == rhs.mCurrNode; };
-		bool operator!= (const BstIteratorBase& rhs) const { return mCurrNode != rhs.mCurrNode; };
-		const T& operator* () const { return mCurrNode->key; };
+        bool operator== (const BstIteratorBase& rhs) const { return mCurrNode == rhs.mCurrNode; };
+        bool operator!= (const BstIteratorBase& rhs) const { return mCurrNode != rhs.mCurrNode; };
+        const T& operator* () const { return mCurrNode->key; };
 
-		// preincrement. move forward to next larger value
-		BstIteratorBase& operator++ () {
-			if (mCurrIndex < mTreeNodeArray.size()) mCurrNode = mTreeNodeArray[++mCurrIndex];
-			return *this;
-		}
+        // preincrement. move forward to next larger value
+        BstIteratorBase& operator++ () {
+            if (mCurrIndex < mTreeNodeArray.size()) mCurrNode = mTreeNodeArray[++mCurrIndex];
+            return *this;
+        }
 
-		// preincrement. move forward to next larger value
-		BstIteratorBase& operator++ (int) {
-			if (mCurrIndex < mTreeNodeArray.size()) mCurrNode = mTreeNodeArray[++mCurrIndex];
-			return *this;
-		}
+        // preincrement. move forward to next larger value
+        BstIteratorBase& operator++ (int) {
+            if (mCurrIndex < mTreeNodeArray.size()) mCurrNode = mTreeNodeArray[++mCurrIndex];
+            return *this;
+        }
 
-		// predecrement. move backward to largest value < current value
-		BstIteratorBase& operator-- () {
-			if (mCurrIndex > 0) mCurrNode = mTreeNodeArray[--mCurrIndex];
-			return *this;
-		}
+        // predecrement. move backward to largest value < current value
+        BstIteratorBase& operator-- () {
+            if (mCurrIndex > 0) mCurrNode = mTreeNodeArray[--mCurrIndex];
+            return *this;
+        }
 
-		// predecrement. move backward to largest value < current value
-		BstIteratorBase& operator-- (int) {
-			if (mCurrIndex > 0) mCurrNode = mTreeNodeArray[--mCurrIndex];
-			return *this;
-		}
+        // predecrement. move backward to largest value < current value
+        BstIteratorBase& operator-- (int) {
+            if (mCurrIndex > 0) mCurrNode = mTreeNodeArray[--mCurrIndex];
+            return *this;
+        }
 
-	protected:
-		vector<TreeNode<T>*> mTreeNodeArray;
-		int mCurrIndex;
-		TreeNode<T> *mCurrNode;
-		virtual void traversing(TreeNode<T> *node) {}
+    protected:
+        vector<TreeNode<T>*> mTreeNodeArray;
+        int mCurrIndex;
+        TreeNode<T> *mCurrNode;
+        virtual void traversing(TreeNode<T> *node) {}
 
-		void setRoot(TreeNode<T> *root) {
-			if (root == NULL) return;
-			traversing(root);
-			mTreeNodeArray.push_back(NULL);
-			mCurrIndex = 0;
-			mCurrNode = mTreeNodeArray[mCurrIndex];
-		}
-	};
+        void setRoot(TreeNode<T> *root) {
+            if (root == NULL) return;
+            traversing(root);
+            mTreeNodeArray.push_back(NULL);
+            mCurrIndex = 0;
+            mCurrNode = mTreeNodeArray[mCurrIndex];
+        }
+    };
 
-	/**
-	* Preorder iterator
-	*/
-	class BstPreorderIterator : public BstIteratorBase {
-	public:
-		BstPreorderIterator() {};
+    /**
+    * Preorder iterator
+    */
+    class BstPreorderIterator : public BstIteratorBase {
+    public:
+        BstPreorderIterator() {};
 
-	private:
-		friend class Bst;
+    private:
+        friend class Bst;
 
-		/**
-		* Traverse in pre-order
-		*/
-		void traversing(TreeNode<T> *root) {
-			// Create an empty stack and push root to it 
-			stack<TreeNode<T>*> nodeStack;
-			nodeStack.push(root);
+        /**
+        * Traverse in pre-order
+        */
+        void traversing(TreeNode<T> *root) {
+            // Create an empty stack and push root to it 
+            stack<TreeNode<T>*> nodeStack;
+            nodeStack.push(root);
 
-			while (!nodeStack.empty())
-			{
-				// Pop the top item from stack and print it 
-				struct TreeNode<T> *node = nodeStack.top();
-				mTreeNodeArray.push_back(node);
-				nodeStack.pop();
+            while (!nodeStack.empty())
+            {
+                // Pop the top item from stack and print it 
+                struct TreeNode<T> *node = nodeStack.top();
+                mTreeNodeArray.push_back(node);
+                nodeStack.pop();
 
-				// Push right and left children of the popped node to stack 
-				if (node->right)
-					nodeStack.push(node->right);
-				if (node->left)
-					nodeStack.push(node->left);
-			}
-		}
-	};
+                // Push right and left children of the popped node to stack 
+                if (node->right)
+                    nodeStack.push(node->right);
+                if (node->left)
+                    nodeStack.push(node->left);
+            }
+        }
+    };
 
-	/**
-	* Inorder iterator
-	*/
-	class BstInorderIterator : public BstIteratorBase {
-	public:
-		BstInorderIterator() {};
+    /**
+    * Inorder iterator
+    */
+    class BstInorderIterator : public BstIteratorBase {
+    public:
+        BstInorderIterator() {};
 
-	private:
-		friend class Bst;
+    private:
+        friend class Bst;
 
-		/**
-		* Traverse in in-order
-		*/
-		void traversing(TreeNode<T> *node) {
-			if (node != NULL) {
-				if (node->left) traversing(node->left);
-				mTreeNodeArray.push_back(node);
-				if (node->right) traversing(node->right);
-			}
-		}
-	};
+        /**
+        * Traverse in in-order
+        */
+        void traversing(TreeNode<T> *node) {
+            if (node != NULL) {
+                if (node->left) traversing(node->left);
+                mTreeNodeArray.push_back(node);
+                if (node->right) traversing(node->right);
+            }
+        }
+    };
 
-	/*
-	* Postorder iterator
-	*/
-	class BstPostorderIterator : public BstIteratorBase {
-	public:
-		BstPostorderIterator() {};
+    /*
+    * Postorder iterator
+    */
+    class BstPostorderIterator : public BstIteratorBase {
+    public:
+        BstPostorderIterator() {};
 
-	private:
-		friend class Bst;
+    private:
+        friend class Bst;
 
-		/**
-		* Traverse in post-order
-		*/
-		void traversing(TreeNode<T> *node) {
-			if (node != NULL) {
-				if (node->left) traversing(node->left);
-				if (node->right) traversing(node->right);
-				mTreeNodeArray.push_back(node);
-			}
-		}
-	};
+        /**
+        * Traverse in post-order
+        */
+        void traversing(TreeNode<T> *node) {
+            if (node != NULL) {
+                if (node->left) traversing(node->left);
+                if (node->right) traversing(node->right);
+                mTreeNodeArray.push_back(node);
+            }
+        }
+    };
 
-	Bst() : mRootNode(NULL) {}
-	~Bst() {
-		auto itIn = this->beginInorder();
-		for (; itIn != this->endInorder(); itIn++) {
-			delete itIn.mCurrNode;
-		}
-	}
+    Bst() : mRootNode(NULL) {}
+    ~Bst() {
+        auto itIn = this->beginInorder();
+        for (; itIn != this->endInorder(); itIn++) {
+            delete itIn.mCurrNode;
+        }
+    }
 
-	/**
-	*  Inserts a new element into the tree (preserving the binary search tree's invariant) 
-	*/
-	void add(T key) {
-		if (mRootNode == NULL) {
-			mRootNode = newNode(key);
-		}
-		else {
-			addInternal(mRootNode, key);
-		}
-	}
+    /**
+    *  Inserts a new element into the tree (preserving the binary search tree's invariant) 
+    */
+    void add(T key) {
+        if (mRootNode == NULL) {
+            mRootNode = newNode(key);
+        }
+        else {
+            addInternal(mRootNode, key);
+        }
+    }
 
-	/**
-	*  Search element by key in the tree
-	*/
-	TreeNode<T>* search(T key) {
-		if (NULL == mRootNode) return NULL;
+    /**
+    *  Search element by key in the tree
+    */
+    TreeNode<T>* search(T key) {
+        if (NULL == mRootNode) return NULL;
 
-		TreeNode<T>* currentNode = mRootNode;
+        TreeNode<T>* currentNode = mRootNode;
 
-		while (currentNode) {
-			if (key < currentNode->key)
-				currentNode = currentNode->left;
-			else if (key > currentNode->key)
-				currentNode = currentNode->right;
-			else
-				return currentNode;
-		}
+        while (currentNode) {
+            if (key < currentNode->key)
+                currentNode = currentNode->left;
+            else if (key > currentNode->key)
+                currentNode = currentNode->right;
+            else
+                return currentNode;
+        }
 
-		return NULL;
-	}
+        return NULL;
+    }
 
-	/**
-	* Returns an iterator referring to the first element in the BST container in pre-order.
-	*/
-	BstPreorderIterator beginPreorder() {
-		BstPreorderIterator it;
-		it.setRoot(mRootNode);
-		return it;
-	};
+    /**
+    * Returns an iterator referring to the first element in the BST container in pre-order.
+    */
+    BstPreorderIterator beginPreorder() {
+        BstPreorderIterator it;
+        it.setRoot(mRootNode);
+        return it;
+    };
 
-	/**
-	* Returns an iterator referring to the past-the-end element in the BST container.
-	*/
-	BstPreorderIterator endPreorder() {
-		BstPreorderIterator it;
-		it.mCurrNode = NULL;
-		return it;
-	}
+    /**
+    * Returns an iterator referring to the past-the-end element in the BST container.
+    */
+    BstPreorderIterator endPreorder() {
+        BstPreorderIterator it;
+        it.mCurrNode = NULL;
+        return it;
+    }
 
-	/**
-	* Returns an iterator referring to the first element in the BST container in in-order.
-	*/
-	BstInorderIterator beginInorder() {
-		BstInorderIterator it;
-		it.setRoot(mRootNode);
-		return it;
-	};
+    /**
+    * Returns an iterator referring to the first element in the BST container in in-order.
+    */
+    BstInorderIterator beginInorder() {
+        BstInorderIterator it;
+        it.setRoot(mRootNode);
+        return it;
+    };
 
-	/**
-	* Returns an iterator referring to the past-the-end element in the BST container.
-	*/
-	BstInorderIterator endInorder() {
-		BstInorderIterator it;
-		it.mCurrNode = NULL;
-		return it;
-	}
+    /**
+    * Returns an iterator referring to the past-the-end element in the BST container.
+    */
+    BstInorderIterator endInorder() {
+        BstInorderIterator it;
+        it.mCurrNode = NULL;
+        return it;
+    }
 
-	/**
-	* Returns an iterator referring to the first element in the BST container in post-order.
-	*/	
-	BstPostorderIterator beginPostorder() {
-		BstPostorderIterator it;
-		it.setRoot(mRootNode);
-		return it;
-	};
+    /**
+    * Returns an iterator referring to the first element in the BST container in post-order.
+    */    
+    BstPostorderIterator beginPostorder() {
+        BstPostorderIterator it;
+        it.setRoot(mRootNode);
+        return it;
+    };
 
-	/**
-	* Returns an iterator referring to the past-the-end element in the BST container.
-	*/
-	BstPostorderIterator endPostorder() {
-		BstPostorderIterator it;
-		it.mCurrNode = NULL;
-		return it;
-	}
+    /**
+    * Returns an iterator referring to the past-the-end element in the BST container.
+    */
+    BstPostorderIterator endPostorder() {
+        BstPostorderIterator it;
+        it.mCurrNode = NULL;
+        return it;
+    }
 
-	TreeNode<T>* getroot() {
-		return mRootNode;
-	}
+    TreeNode<T>* getroot() {
+        return mRootNode;
+    }
 };
 
 #endif // _BST_H_
--- UnitTest.cpp
+++ UnitTest.cpp
@@ -1,8 +1,8 @@
 /**
-* @file		UnitTest.cpp
-* @author	TuanPM3
-* @date		12/Nov/2018
-* @brief	Binary Search Tree UnitTest
+* @file        UnitTest.cpp
+* @author    TuanPM3
+* @date        12/Nov/2018
+* @brief    Binary Search Tree UnitTest
 */
 
 //#include "stdafx.h"
@@ -16,231 +16,231 @@ using namespace std;
 template<typename T>
 bool compareVector(vector<T> v1, vector<T> v2)
 {
-	if (v1.size() != v2.size()) return false;
-	for (int i = 0; i < v1.size(); i++) {
-		if (v1[i] != v2[i]) return false;
-	}
-	return true;
+    if (v1.size() != v2.size()) return false;
+    for (int i = 0; i < v1.size(); i++) {
+        if (v1[i] != v2[i]) return false;
+    }
+    return true;
 }
 
 /*================ UT for add() method ====================*/
 void TEST1()
 {
-	cout << __FUNCTION__ << ": ";
-	Bst<int> bt;
-	bt.add(6);
-	bt.add(4);
-	bt.add(8);
-	bt.add(2);
-	bt.add(5);
-	bt.add(7);
-	bt.add(9);
-	TreeNode<int> *root = bt.getroot();
-	assert(root->key == 6);
-	assert(root->left->key == 4);
-	assert(root->right->key == 8);
-	assert(root->left->left->key == 2);
-	assert(root->left->right->key == 5);
-	assert(root->right->left->key == 7);
-	assert(root->right->right->key == 9);
-	cout << "OK" << endl;
+    cout << __FUNCTION__ << ": ";
+    Bst<int> bt;
+    bt.add(6);
+    bt.add(4);
+    bt.add(8);
+    bt.add(2);
+    bt.add(5);
+    bt.add(7);
+    bt.add(9);
+    TreeNode<int> *root = bt.getroot();
+    assert(root->key == 6);
+    assert(root->left->key == 4);
+    assert(root->right->key == 8);
+    assert(root->left->left->key == 2);
+    assert(root->left->right->key == 5);
+    assert(root->right->left->key == 7);
+    assert(root->right->right->key == 9);
+    cout << "OK" << endl;
 }
 
 void TEST2()
 {
-	cout << __FUNCTION__ << ": ";
-	Bst<float> bt;
-	bt.add(5.0);	
-	bt.add(4.0);
-	bt.add(2.0);
-	bt.add(6.0);
-	bt.add(8.0);
-	bt.add(7.0);	
-	bt.add(9.0);
-	TreeNode<float> *root = bt.getroot();
-	assert(root->key == 5.0);
-	assert(root->left->key == 4.0);
-	assert(root->right->key == 6.0);
-	assert(root->left->left->key == 2.0);
-	assert(root->right->right->key == 8.0);
-	assert(root->right->right->left->key == 7.0);
-	assert(root->right->right->right->key == 9.0);
-	cout << "OK" << endl;
+    cout << __FUNCTION__ << ": ";
+    Bst<float> bt;
+    bt.add(5.0);    
+    bt.add(4.0);
+    bt.add(2.0);
+    bt.add(6.0);
+    bt.add(8.0);
+    bt.add(7.0);    
+    bt.add(9.0);
+    TreeNode<float> *root = bt.getroot();
+    assert(root->key == 5.0);
+    assert(root->left->key == 4.0);
+    assert(root->right->key == 6.0);
+    assert(root->left->left->key == 2.0);
+    assert(root->right->right->key == 8.0);
+    assert(root->right->right->left->key == 7.0);
+    assert(root->right->right->right->key == 9.0);
+    cout << "OK" << endl;
 }
 
 void TEST3()
 {
-	cout << __FUNCTION__ << ": ";
-	Bst<string> bt;
-	bt.add("March");
-	bt.add("January");
-	bt.add("October");
-	bt.add("August");
-	bt.add("June");
-	bt.add("May");
-	bt.add("September");
-	TreeNode<string> *root = bt.getroot();
-	assert(root->key == "March");
-	assert(root->left->key == "January");
-	assert(root->right->key == "October");
-	assert(root->left->left->key == "August");
-	assert(root->left->right->key == "June");
-	assert(root->right->left->key == "May");
-	assert(root->right->right->key == "September");
-	cout << "OK" << endl;
+    cout << __FUNCTION__ << ": ";
+    Bst<string> bt;
+    bt.add("March");
+    bt.add("January");
+    bt.add("October");
+    bt.add("August");
+    bt.add("June");
+    bt.add("May");
+    bt.add("September");
+    TreeNode<string> *root = bt.getroot();
+    assert(root->key == "March");
+    assert(root->left->key == "January");
+    assert(root->right->key == "October");
+    assert(root->left->left->key == "August");
+    assert(root->left->right->key == "June");
+    assert(root->right->left->key == "May");
+    assert(root->right->right->key == "September");
+    cout << "OK" << endl;
 }
 
 /*================ UT for search() method ====================*/
 void TEST4()
 {
-	cout << __FUNCTION__ << ": ";
-	Bst<int> bt;
-	bt.add(6);
-	bt.add(4);
-	bt.add(8);
-	bt.add(2);
-	bt.add(5);
-	bt.add(7);
-	bt.add(9);
+    cout << __FUNCTION__ << ": ";
+    Bst<int> bt;
+    bt.add(6);
+    bt.add(4);
+    bt.add(8);
+    bt.add(2);
+    bt.add(5);
+    bt.add(7);
+    bt.add(9);
 
-	int arr[] = { 2,4,5,6,7,8,9 };
-	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
-		assert(bt.search(arr[i])->key == arr[i]);
-	}	
-	assert(bt.search(10) == NULL);
-	cout << "OK" << endl;
+    int arr[] = { 2,4,5,6,7,8,9 };
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+        assert(bt.search(arr[i])->key == arr[i]);
+    }    
+    assert(bt.search(10) == NULL);
+    cout << "OK" << endl;
 }
 
 void TEST5()
 {
-	cout << __FUNCTION__ << ": ";
-	Bst<float> bt;
-	bt.add(5.0);
-	bt.add(4.0);
-	bt.add(2.0);
-	bt.add(6.0);
-	bt.add(8.0);
-	bt.add(7.0);
-	bt.add(9.0);
-	float arr[] = { 2.0,4.0,5.0,6.0,7.0,8.0,9.0 };
-	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
-		assert(bt.search(arr[i])->key == arr[i]);
-	}
-	assert(bt.search(10.0) == NULL);
-	cout << "OK" << endl;
+    cout << __FUNCTION__ << ": ";
+    Bst<float> bt;
+    bt.add(5.0);
+    bt.add(4.0);
+    bt.add(2.0);
+    bt.add(6.0);
+    bt.add(8.0);
+    bt.add(7.0);
+    bt.add(9.0);
+    float arr[] = { 2.0,4.0,5.0,6.0,7.0,8.0,9.0 };
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+        assert(bt.search(arr[i])->key == arr[i]);
+    }
+    assert(bt.search(10.0) == NULL);
+    cout << "OK" << endl;
 }
 
 void TEST6()
 {
-	cout << __FUNCTION__ << ": ";
-	Bst<string> bt;
-	bt.add("March");
-	bt.add("January");
-	bt.add("October");
-	bt.add("August");
-	bt.add("June");
-	bt.add("May");
-	bt.add("September");
-	string arr[] = { 
-		"March",
-		"January",
-		"October",
-		"August",
-		"June",
-		"May",
-		"September"
-	};
-	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
-		assert(bt.search(arr[i])->key == arr[i]);
-	}
-	assert(bt.search("Not Found") == NULL);
-	cout << "OK" << endl;
+    cout << __FUNCTION__ << ": ";
+    Bst<string> bt;
+    bt.add("March");
+    bt.add("January");
+    bt.add("October");
+    bt.add("August");
+    bt.add("June");
+    bt.add("May");
+    bt.add("September");
+    string arr[] = { 
+        "March",
+        "January",
+        "October",
+        "August",
+        "June",
+        "May",
+        "September"
+    };
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+        assert(bt.search(arr[i])->key == arr[i]);
+    }
+    assert(bt.search("Not Found") == NULL);
+    cout << "OK" << endl;
 }
 
 /*================ UT for preorder iterator ====================*/
 void TEST7()
 {
-	cout << __FUNCTION__ << ": ";
-	Bst<int> bt;
-	bt.add(6);
-	bt.add(4);
-	bt.add(8);
-	bt.add(2);
-	bt.add(5);
-	bt.add(7);
-	bt.add(9);
-	vector<int> actual;
-	auto itPre = bt.beginPreorder();
-	for (; itPre != bt.endPreorder(); itPre++) {
-		actual.push_back(*itPre);
-	}
-	vector<int> expect = { 6, 4, 2, 5, 8, 7, 9 };
-	assert(compareVector(actual, expect));
-	cout << "OK" << endl;
+    cout << __FUNCTION__ << ": ";
+    Bst<int> bt;
+    bt.add(6);
+    bt.add(4);
+    bt.add(8);
+    bt.add(2);
+    bt.add(5);
+    bt.add(7);
+    bt.add(9);
+    vector<int> actual;
+    auto itPre = bt.beginPreorder();
+    for (; itPre != bt.endPreorder(); itPre++) {
+        actual.push_back(*itPre);
+    }
+    vector<int> expect = { 6, 4, 2, 5, 8, 7, 9 };
+    assert(compareVector(actual, expect));
+    cout << "OK" << endl;
 }
 
 void TEST8()
 {
-	cout << __FUNCTION__ << ": ";
-	Bst<int> bt;
-	bt.add(6);
-	bt.add(4);
-	bt.add(8);
-	bt.add(2);
-	bt.add(5);
-	bt.add(7);
-	bt.add(9);
-	vector<int> actual;
-	auto itIn = bt.beginInorder();
-	for (; itIn != bt.endInorder(); itIn++) {
-		actual.push_back(*itIn);
-	}
-	vector<int> expect = { 2, 4, 5, 6, 7, 8, 9 };
-	assert(compareVector(actual, expect));
-	cout << "OK" << endl;
+    cout << __FUNCTION__ << ": ";
+    Bst<int> bt;
+    bt.add(6);
+    bt.add(4);
+    bt.add(8);
+    bt.add(2);
+    bt.add(5);
+    bt.add(7);
+    bt.add(9);
+    vector<int> actual;
+    auto itIn = bt.beginInorder();
+    for (; itIn != bt.endInorder(); itIn++) {
+        actual.push_back(*itIn);
+    }
+    vector<int> expect = { 2, 4, 5, 6, 7, 8, 9 };
+    assert(compareVector(actual, expect));
+    cout << "OK" << endl;
 }
 
 void TEST9()
 {
-	cout << __FUNCTION__ << ": ";
-	Bst<int> bt;
-	bt.add(6);
-	bt.add(4);
-	bt.add(8);
-	bt.add(2);
-	bt.add(5);
-	bt.add(7);
-	bt.add(9);
-	vector<int> actual;
-	auto itPost = bt.beginPostorder();
-	for (; itPost != bt.endPostorder(); itPost++) {
-		actual.push_back(*itPost);
-	}
-	vector<int> expect = { 2, 5, 4, 7, 9, 8, 6 };
-	assert(compareVector(actual, expect));
-	cout << "OK" << endl;
+    cout << __FUNCTION__ << ": ";
+    Bst<int> bt;
+    bt.add(6);
+    bt.add(4);
+    bt.add(8);
+    bt.add(2);
+    bt.add(5);
+    bt.add(7);
+    bt.add(9);
+    vector<int> actual;
+    auto itPost = bt.beginPostorder();
+    for (; itPost != bt.endPostorder(); itPost++) {
+        actual.push_back(*itPost);
+    }
+    vector<int> expect = { 2, 5, 4, 7, 9, 8, 6 };
+    assert(compareVector(actual, expect));
+    cout << "OK" << endl;
 }
 
 int main()
 {
-	/*
-	* I write this UT for demo how to UT without UT Framework
-	* These are not full UT testcase
-	*/
-	cout << "================ UT for add() method ====================" << endl;
-	TEST1();
-	TEST2();
-	TEST3();
-	cout << "================ UT for search() method ====================" << endl;
-	TEST4();
-	TEST5();
-	TEST6();
-	cout << "/*================ UT for preorder iterator ====================*/" << endl;
-	TEST7();
-	cout << "/*================ UT for inorder iterator ====================*/" << endl;
-	TEST8();
-	cout << "/*================ UT for postorder iterator ====================*/" << endl;
-	TEST9();
+    /*
+    * I write this UT for demo how to UT without UT Framework
+    * These are not full UT testcase
+    */
+    cout << "================ UT for add() method ====================" << endl;
+    TEST1();
+    TEST2();
+    TEST3();
+    cout << "================ UT for search() method ====================" << endl;
+    TEST4();
+    TEST5();
+    TEST6();
+    cout << "/*================ UT for preorder iterator ====================*/" << endl;
+    TEST7();
+    cout << "/*================ UT for inorder iterator ====================*/" << endl;
+    TEST8();
+    cout << "/*================ UT for postorder iterator ====================*/" << endl;
+    TEST9();
     return 0;
 }
 
